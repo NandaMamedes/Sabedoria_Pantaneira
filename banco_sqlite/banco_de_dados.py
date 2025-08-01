49079ceab2ca8e69d84832488091877a306a36e0
prg-valel <arthur.castro@sou.ufmt.br>
2025-08-01 10:19:00 -0400

    .

--- banco_sqlite/banco_de_dados.py
+++ banco_sqlite/banco_de_dados.py
@@ -17,7 +17,6 @@ from perguntas_cultura import perguntas_cultura
 from perguntas_variedades import perguntas_variedades
 from perguntas_meio_ambiente import perguntas_meio_ambiente
 from perguntas_politica import perguntas_politica
-from logica_de_negocio import regras_jogo
 from dataclasses import dataclass
 
 @dataclass
@@ -137,5 +136,3 @@ def adicionar_apelido(j: Jogador):
         conn.execute("INSERT INTO ranking_local (jogador, pontuacao, nivel, categoria) VALUES (?,?,?,?)",
                      (j.jogador, "N/A", "N/A", "N/A", "N/A"))
         conn.commit()
-
-regras_jogo.verificar_resposta()
