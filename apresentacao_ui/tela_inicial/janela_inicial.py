6bcc09493e778f5ccc05345852128b5af2e9d9ea
NandaMamedes <nandamamedes100@gmail.com>
2025-08-13 10:17:20 -0400

    Correção das Janelas

--- apresentacao_ui/tela_inicial/janela_inicial.py
+++ apresentacao_ui/tela_inicial/janela_inicial.py
@@ -14,10 +14,28 @@ from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QDialog
 from banco_sqlite.banco_de_dados import adicionar_apelido, obter_pergunta_aleatoria_por_dificuldade, obter_opcoes, obter_resposta
 
+############################### foi adicionado ####################################
+from dotenv import load_dotenv
+
+## GAMBIARRA TEMPORARIA 
+interface_tela_onca = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_inicial_onça.ui"
+interface_tela_arara = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_inicial_arara.ui"
+interface_tela_capivara = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_inicial_capivara.ui"
+interface_tela_jacare = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_inicial_jacare.ui"
+interface_tela_tuiuiui = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_inicial_tuiuiu.ui"
+interface_tela_apelido = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_apelido.ui"
+interface_tela_pergunta = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_pergunta.ui"
+interface_tela_confirmar = "C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_confirmar.ui"
+
+
+
 class TelaInicialArara(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_inicial_arara.ui", self)
+
+        #uic.loadUi("C:/projeto_software/tela_inicial_onça.ui", self)
+        uic.loadUi(interface_tela_arara, self) # TESTE
+       
         self.label_jogar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_placar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_jogar.mousePressEvent = self.abrir_tela_apelido
@@ -35,7 +53,9 @@ class TelaInicialArara(QDialog):
 class TelaInicialOnca(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_inicial_onça.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_inicial_onça.ui", self)
+        uic.loadUi(interface_tela_onca, self) # TESTE
+
         self.label_jogar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_placar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_jogar.mousePressEvent = self.abrir_tela_apelido
@@ -53,7 +73,8 @@ class TelaInicialOnca(QDialog):
 class TelaInicialCapivara(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_inicial_capivara.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_inicial_capivara.ui", self)
+        uic.loadUi(interface_tela_capivara, self) # TESTE
         self.label_jogar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_placar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_jogar.mousePressEvent = self.abrir_tela_apelido
@@ -70,7 +91,8 @@ class TelaInicialCapivara(QDialog):
 class TelaInicialJacare(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_inicial_jacaré.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_inicial_jacaré.ui", self)
+        uic.loadUi(interface_tela_jacare, self) # TESTE
         self.label_jogar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_placar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_jogar.mousePressEvent = self.abrir_tela_apelido
@@ -88,7 +110,8 @@ class TelaInicialJacare(QDialog):
 class TelaInicialTuiuiu(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_inicial_tuiuiu.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_inicial_tuiuiu.ui", self)
+        uic.loadUi(interface_tela_tuiuiu, self) # TESTE
         self.label_jogar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_placar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_jogar.mousePressEvent = self.abrir_tela_apelido
@@ -106,9 +129,11 @@ class TelaInicialTuiuiu(QDialog):
 class TelaApelido(QDialog):
     def __init__(self, tela_inicial):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_apelido.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_apelido.ui", self)
+        uic.loadUi(interface_tela_apelido, self) # TESTE
         self.tela_inicial = tela_inicial
-        self.label_imagem.setPixmap(QPixmap("C:/projeto_software/janela_apelido.png"))
+        #self.label_imagem.setPixmap(QPixmap("C:/projeto_software/janela_apelido.png"))
+        #self.label_imagem.setPixmap(QPixmap("C:/Users/cyber_edux__109/Documents/GitHub/Sabedoria_Pantaneira/apresentacao_ui/tela_inicial/janelas_tela_inicial/tela_apelido.png"))
         self.label_imagem.setScaledContents(True)
         self.label_continuar.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_continuar.mousePressEvent = self.verificar_apelido
@@ -133,7 +158,8 @@ perguntas_dificeis = []
 class TelaPergunta(QDialog):
     def __init__(self, tela_anterior):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_pergunta.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_pergunta.ui", self) #####################
+        uic.loadUi(interface_tela_pergunta, self) # TESTE
         self.tela_anterior = tela_anterior
 
         self.pergunta = obter_pergunta_aleatoria_por_dificuldade("fácil")
@@ -151,7 +177,13 @@ class TelaPergunta(QDialog):
         
         font_pergunta = QFont("Arial", 13, QFont.Weight.Bold)
         font_opcoes = QFont("Arial", 12)
-        
+
+        ##################  CÓDIGO QUEBRA DE LINHA ###################################
+        self.label_pergunta.setFont(font_pergunta)
+        self.label_pergunta.setWordWrap(True)
+        self.label_pergunta.setText(self.pergunta)
+        ###########################################################################
+
         self.label_pergunta.setFont(font_pergunta)
         for lbl in [self.label_a, self.label_b, self.label_c, self.label_d]:
             lbl.setFont(font_opcoes)
@@ -178,7 +210,8 @@ class TelaPergunta(QDialog):
 class TelaConfirmar(QDialog):
     def __init__(self, tela_anterior, pergunta, resposta_escolhida):
         super().__init__()
-        uic.loadUi("C:/projeto_software/tela_confirmar.ui", self)
+        #uic.loadUi("C:/projeto_software/tela_confirmar.ui", self)
+        uic.loadUi(interface_tela_confirmar, self) #TESTE
         self.tela_anterior = tela_anterior
         self.pergunta = pergunta
         self.resposta_escolhida = resposta_escolhida
